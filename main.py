4325c1117a40e207a5f86f21d6859e7fb7aea026
AurelioLouback <carvalho.marco@pucpr.edu.br>
2025-04-21 14:19:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-#Comentario - Gerar alteracao
+
 @app.get("/teste1")
 async def funcaoteste():
     return {"teste":"Deu certo"}
